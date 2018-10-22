28ce9c51b7538ba39f7548b0d3d924641517524c
surekha <surekhasrao@gmail.com>
2018-10-22 22:32:08 +0000

    modified server.js

--- expresstest/server.js
+++ expresstest/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts);
 
 app.get('/', (request, response) => response.send('<h1>hello</h1>'));
 
@@ -11,6 +19,18 @@ app.get('/test/add/:num', (req, res) => {
     const num = parseInt(req.params.num, 10);
     res.send(`<h1>${5 + num}</h1>`);
 });
+
+//query parameter under req.query object
+app.get('/test/subtract', (req, res) => {
+    console.log(req.query);
+    const num = parseInt(req.query.num, 10);
+    res.send(`<h1>${num - 5}</h1>`);
+});
+
+app.post('/test/multiply', (req, res) => {
+    const num = parseInt(req.body.num, 10);
+    res.json({result: num * 5});
+})
 
 const port = process.env.PORT || 5000;
 
