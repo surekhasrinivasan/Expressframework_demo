a1a91878b91fbbe8aa315c1e6a56585d35066d5b
surekha <surekhasrao@gmail.com>
2018-10-23 15:41:34 +0000

    Modified server.js, all the routes working

--- expresstest/server.js
+++ expresstest/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const posts = require('./routes/api/posts');
-
 app.use('/api/posts', posts);
 
 app.get('/', (request, response) => response.send('<h1>hello</h1>'));
 
 app.get('/test', (req, res) => res.send('<h1> test route</h1>'));
 
-app.get('/test/:num', (req, res) => res.send(`<h1>${req.params.num}</h1>`));
+//app.get('/test/:num', (req, res) => res.send(`<h1>${req.params.num}</h1>`));
 
 app.get('/test/add/:num', (req, res) => {
     const num = parseInt(req.params.num, 10);
@@ -22,8 +22,8 @@ app.get('/test/add/:num', (req, res) => {
 
 //query parameter under req.query object
 app.get('/test/subtract', (req, res) => {
-    console.log(req.query);
     const num = parseInt(req.query.num, 10);
+    console.log(num);
     res.send(`<h1>${num - 5}</h1>`);
 });
 
